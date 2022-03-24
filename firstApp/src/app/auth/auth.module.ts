324c40f1914bb92289403fffb684890e5d7d5517
VladimirParmon <parmon_vladimir@bk.ru>
2022-03-24 20:06:19 +0300

    add login validation (brick and mortar)

--- firstApp/src/app/auth/auth.module.ts
+++ firstApp/src/app/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { CoreModule } from '../core/core.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { FormsModule } from '@angular/forms';
 import { authRoutingModule } from './auth-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginPageComponent],
-  imports: [CommonModule, CoreModule, FormsModule, authRoutingModule],
+  imports: [CommonModule, CoreModule, FormsModule, authRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
--- firstApp/src/app/youtube/pages/admin/admin.component.ts
+++ firstApp/src/app/youtube/pages/admin/admin.component.ts
@@ -8,6 +8,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class AdminComponent implements OnInit {
   reg = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+
   createForm = this.fb.group({
     title: ['', Validators.compose([Validators.minLength(3), Validators.maxLength(20), Validators.required])],
     description: ['', Validators.maxLength(255)],
