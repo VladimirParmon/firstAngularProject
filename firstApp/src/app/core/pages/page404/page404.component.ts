c5d9b98f07b637abe61babbee46ead2d2f32c713
VladimirParmon <parmon_vladimir@bk.ru>
2022-04-10 15:13:50 +0300

    add 404 page

---
+++ firstApp/src/app/core/pages/page404/page404.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page404',
+  templateUrl: './page404.component.html',
+  styleUrls: ['./page404.component.scss'],
+})
+export class Page404Component {}
