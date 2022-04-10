33ac17a2e9223462945fbd06a69e0c33948ec24b
VladimirParmon <parmon_vladimir@bk.ru>
2022-04-10 15:35:44 +0300

    squash! add missing core modules files

--- firstApp/src/app/youtube/youtube.module.ts
+++ firstApp/src/app/youtube/youtube.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { YoutubeRoutingModule } from './youtube-routing.module';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { DetailedInfoPageComponent } from './pages/detailed-info-page/detailed-info-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainPageComponent,
+    DetailedInfoPageComponent
+  ],
   imports: [
     CommonModule,
     YoutubeRoutingModule
