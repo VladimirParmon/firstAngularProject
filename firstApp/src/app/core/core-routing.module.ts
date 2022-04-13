1612e3ae0a34fa5d999feb71720bc9f6ca3240da
VladimirParmon <parmon_vladimir@bk.ru>
2022-04-13 19:52:11 +0300

    block 404 for unauthorized users

--- firstApp/src/app/core/core-routing.module.ts
+++ firstApp/src/app/core/core-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GeneralRoutingGuard } from './guards/general-routing.guard';
 import { Page404Component } from './pages/page404/page404.component';
 
 const routes: Routes = [
   {
     path: '',
     component: Page404Component,
+    canActivate: [GeneralRoutingGuard],
+    canLoad: [GeneralRoutingGuard],
   },
 ];
 
