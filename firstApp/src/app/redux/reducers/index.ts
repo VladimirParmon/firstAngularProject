8fb45c05231d50f97a757fc603db2e9ec9432774
VladimirParmon <parmon_vladimir@bk.ru>
2022-04-17 15:55:52 +0300

    define initial store state

--- firstApp/src/app/redux/reducers/index.ts
+++ firstApp/src/app/redux/reducers/index.ts
@@ -1,8 +1,18 @@
-import { ActionReducer, ActionReducerMap, createFeatureSelector, createSelector, MetaReducer } from '@ngrx/store';
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { environment } from '../../../environments/environment';
+import { APIVideosReducer } from './APIVideos.reducers';
+import { customCardsReducer } from './customCards.reducers';
 
-export interface State {}
+export interface State {
+  APIVideosState: any;
+  customCardsState: any;
+}
 
-export const reducers: ActionReducerMap<State> = {};
+export const initialState = {};
+
+export const reducers: ActionReducerMap<State> = {
+  APIVideosState: APIVideosReducer,
+  customCardsState: customCardsReducer,
+};
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
