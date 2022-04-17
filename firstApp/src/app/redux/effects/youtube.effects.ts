fd21467f02056839cd7f346abd8bd2c53f255616
VladimirParmon <parmon_vladimir@bk.ru>
2022-04-17 15:52:04 +0300

    add effect that listenes for the search action

---
+++ firstApp/src/app/redux/effects/youtube.effects.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { loadAPIVideos, loadAPIVideosFailure, loadAPIVideosSuccess } from '../actions/youtube.actions';
+import { DataService } from '../../youtube/services/data.service';
+import { catchError, debounceTime, distinctUntilChanged, EMPTY, map, mergeMap, of, switchMap } from 'rxjs';
+
+@Injectable()
+export class YoutubeEffects {
+  getDataFromYoutubeAPI$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(loadAPIVideos),
+        debounceTime(1000),
+        distinctUntilChanged(),
+        mergeMap((action) =>
+          this.service.getVideos(action.string).pipe(
+            map((data) => loadAPIVideosSuccess(data)),
+            catchError((error) => of(loadAPIVideosFailure({ error: error })))
+          )
+        )
+      )
+    // { dispatch: false }
+  );
+
+  constructor(private actions$: Actions, public service: DataService) {}
+}
