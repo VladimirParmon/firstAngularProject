ba8c11ab5d56cc7776cad8f7c643ce799d9bb8a4
VladimirParmon <parmon_vladimir@bk.ru>
2022-04-20 21:44:18 +0300

    detailed page rework: it's using ngrx store now to fetch info and displays 403 error in DOM

--- firstApp/src/app/youtube/pages/detailed-info-page/detailed-info-page.component.ts
+++ firstApp/src/app/youtube/pages/detailed-info-page/detailed-info-page.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { VideoItem } from 'src/app/models/youtubeResponse';
-import { DataService } from '../../services/data.service';
 import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { loadSingleAPIVideo } from 'src/app/redux/actions/youtube.actions';
+import { detailedVideoInfoStateData, selectErrorMessageInfo } from 'src/app/redux/selectors/youtube.selectors';
+import { VideoItem } from 'src/app/models/youtubeResponse';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-detailed-info-page',
@@ -9,17 +13,26 @@ import { Location } from '@angular/common';
   styleUrls: ['./detailed-info-page.component.scss'],
 })
 export class DetailedInfoPageComponent implements OnInit {
-  videoInfo!: VideoItem;
+  videoId: string;
+  videoInfo: VideoItem | null = null;
   imageUrl: string = '';
+  errorSpan$: Observable<string>;
 
-  constructor(private service: DataService, private location: Location) {
-    this.videoInfo = this.service.detailedVideoInfo;
+  constructor(private location: Location, private route: ActivatedRoute, private store: Store) {
+    this.videoId = this.route.snapshot.params['videoId'];
+    this.errorSpan$ = this.store.select(selectErrorMessageInfo);
   }
 
   ngOnInit(): void {
-    this.imageUrl = this.videoInfo.snippet.thumbnails['maxres']?.url
-      ? this.videoInfo.snippet.thumbnails['maxres'].url
-      : this.videoInfo.snippet.thumbnails['standard'].url;
+    this.store.dispatch(loadSingleAPIVideo({ string: [this.videoId] }));
+    this.store.select(detailedVideoInfoStateData).subscribe((data) => {
+      this.videoInfo = data;
+    });
+    this.store.select(detailedVideoInfoStateData).subscribe((data) => {
+      this.imageUrl = data?.snippet?.thumbnails['maxres']
+        ? data?.snippet?.thumbnails['maxres'].url
+        : data?.snippet?.thumbnails['standard'].url;
+    });
   }
 
   goBack(): void {
