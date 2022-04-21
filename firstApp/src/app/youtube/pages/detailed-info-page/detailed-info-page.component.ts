c34b8831345f458261e680c159628a490958748d
VladimirParmon <parmon_vladimir@bk.ru>
2022-04-21 10:24:40 +0300

    fix: unsubscribe in detailed info page

--- firstApp/src/app/youtube/pages/detailed-info-page/detailed-info-page.component.ts
+++ firstApp/src/app/youtube/pages/detailed-info-page/detailed-info-page.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+import { VideoItem } from 'src/app/models/youtubeResponse';
 import { loadSingleAPIVideo } from 'src/app/redux/actions/youtube.actions';
 import { detailedVideoInfoStateData, selectErrorMessageInfo } from 'src/app/redux/selectors/youtube.selectors';
-import { VideoItem } from 'src/app/models/youtubeResponse';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-detailed-info-page',
@@ -17,6 +17,8 @@ export class DetailedInfoPageComponent implements OnInit {
   videoInfo: VideoItem | null = null;
   imageUrl: string = '';
   errorSpan$: Observable<string>;
+  detailedVideoInfoDataSubscription_Video$!: Subscription;
+  detailedVideoInfoDataSubscription_imageUrl$!: Subscription;
 
   constructor(private location: Location, private route: ActivatedRoute, private store: Store) {
     this.videoId = this.route.snapshot.params['videoId'];
@@ -25,14 +27,21 @@ export class DetailedInfoPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.store.dispatch(loadSingleAPIVideo({ string: [this.videoId] }));
-    this.store.select(detailedVideoInfoStateData).subscribe((data) => {
+    this.detailedVideoInfoDataSubscription_Video$ = this.store.select(detailedVideoInfoStateData).subscribe((data) => {
       this.videoInfo = data;
     });
-    this.store.select(detailedVideoInfoStateData).subscribe((data) => {
-      this.imageUrl = data?.snippet?.thumbnails['maxres']
-        ? data?.snippet?.thumbnails['maxres'].url
-        : data?.snippet?.thumbnails['standard'].url;
-    });
+    this.detailedVideoInfoDataSubscription_imageUrl$ = this.store
+      .select(detailedVideoInfoStateData)
+      .subscribe((data) => {
+        this.imageUrl = data?.snippet?.thumbnails['maxres']
+          ? data?.snippet?.thumbnails['maxres'].url
+          : data?.snippet?.thumbnails['standard'].url;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.detailedVideoInfoDataSubscription_Video$.unsubscribe();
+    this.detailedVideoInfoDataSubscription_imageUrl$.unsubscribe();
   }
 
   goBack(): void {
